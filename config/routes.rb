38000d5dc25fba96feb9fe2fc4e2b7dc81623045
Bebop28 <samomsebi28@gmail.com>
2018-02-08 22:32:32 +0100

    Use Ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
+
   devise_for :users
   root 'welcome#index'
-  get 'my_portfolio', to: "users#my_portfolio"
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_path', to: 'stocks#search'
 end
