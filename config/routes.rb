7fcc8c125bc2aaa515e6d2435a5e78e36a617a04
Bebop28 <samomsebi28@gmail.com>
2018-02-26 12:41:49 +0100

    Add the stock link  to my_portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'user_stocks/create'
+
   get 'stocks/search'
 
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_path', to: 'stocks#search'
+  resources :user_stocks, only: [:create]
 end
