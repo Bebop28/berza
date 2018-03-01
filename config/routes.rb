feac0e55da54f83c81f95010cbf1593cc0f353ac
Bebop28 <samomsebi28@gmail.com>
2018-03-01 16:58:19 +0100

    My friends search and list

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'user_stocks/create'
-
-  get 'stocks/search'
-
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :frienships
 
+  
+  get 'user_stocks/create'
+  get 'stocks/search'
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_path', to: 'stocks#search'
-  resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
 end
